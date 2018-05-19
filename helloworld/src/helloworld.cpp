baf68b91690d88f6c7cd6204c71c22ee68b6bca1
ubuntuLiuYang <15771776811@163.com>
2018-05-20 03:58:27 +0800

    对C++改进，加入了打印函数

--- helloworld/src/helloworld.cpp
+++ helloworld/src/helloworld.cpp
@@ -9,7 +9,18 @@
 #include <iostream>
 using namespace std;
 
+void printSentence(string insentence)
+{
+	cout << insentence << endl;
+}
+
+
 int main() {
+
+	string myFirstSentence = "I love you , Bai Jing !";
+
+	printSentence(myFirstSentence);
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
